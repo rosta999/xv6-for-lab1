0da7f335b97d1fba30dd7120321552f903304543
rosta999 <43803039+rosta999@users.noreply.github.com>
2018-10-30 10:40:24 -0700

    Update defs.h

--- defs.h
+++ defs.h
@@ -120,6 +120,9 @@ void            userinit(void);
 int             wait(void);
 void            wakeup(void*);
 void            yield(void);
+//added
+int             cps(void);
+int             chpr(int pid, int priority);
 
 // swtch.S
 void            swtch(struct context**, struct context*);
