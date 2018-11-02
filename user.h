2288451f9b9734339b020b61ce70ab7886bc1665
rosta999 <43803039+rosta999@users.noreply.github.com>
2018-11-01 21:28:17 -0700

    Update user.h

--- user.h
+++ user.h
@@ -3,8 +3,8 @@ struct rtcdate;
 
 // system calls
 int fork(void);
-int exit(void) __attribute__((noreturn));
-int wait(void);
+int exit(int) __attribute__((noreturn));
+int wait(int*);
 int pipe(int*);
 int write(int, const void*, int);
 int read(int, void*, int);
@@ -24,6 +24,7 @@ char* sbrk(int);
 int sleep(int);
 int uptime(void);
 //added
+int waitpid(int, int*, int);
 int cps(void);
 int chpr(int pid, int priority);
 
