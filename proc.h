df0e0c84baaf57c2a31a86b11c1b8e8124550edf
rosta999 <43803039+rosta999@users.noreply.github.com>
2018-11-01 21:24:51 -0700

    Update proc.h

--- proc.h
+++ proc.h
@@ -49,6 +49,7 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
+  int exitstatus;
   int priority;                // priority 0-30
 };
 
